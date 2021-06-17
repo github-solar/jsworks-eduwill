346170cbb158dc1c90e15385f5a4b0945c719516
github-solar <solarsun@nate.com>
2021-06-17 22:32:11 +0900

    javascript study at eduwill

---
+++ ch04function/js/event.js
@@ -0,0 +1,18 @@
+function showTxt() {
+    // var text = document.getElementById("desc");
+    // text.style.display = "block";
+    document.getElementById("desc").style.display = "block";
+
+    // var button = document.getElementById("btn");
+    // button.style.display = "none"; 
+    document.getElementById("btn").style.display = "none";
+}
+
+function hideTxt() {
+    var text = document.getElementById("desc");
+    text.style.display = "none";
+
+    var button = document.getElementById("btn");
+    button.style.display = "block"; 
+
+}
